3deec116474a50a8c7af83fd89b77f10a3fb7511
Saksham Narang <sakshamnarang961@gmail.com>
2026-06-21 15:33:01 +0530

    Add files via upload

---
+++ ArrayMethods1.js
@@ -0,0 +1,14 @@
+//push(): Adds one or more elements to the end of an array.
+
+let Marvels = ["IronMan", "spiderman", "Hulk"]
+console.log(Marvels)
+Marvels.push("Caption", "Thor")
+
+//pop(): Removes the last element from an array and returns it.
+
+let deletedItem = Marvels.pop()
+console.log("Deleted item :", deletedItem)
+
+//toString() method converts an array into a string.
+
+console.log(Marvels.toString())
