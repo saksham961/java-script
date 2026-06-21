107b54d8ea7258b0ec5fa3d041fdf1c4eb46eba8
Saksham Narang <sakshamnarang961@gmail.com>
2026-06-21 15:30:16 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,29 @@
+console.log(55+55)
+
+console.log("saksham")
+
+age=18*2
+console.log("your age =", age)
+
+p = "Good"
+q = " Morning"
+console.log(p + q)
+
+let name = "Narang"
+console.log(name)
+
+// blocks
+
+ const student = {
+    Name : "saksham",
+    class :12 ,
+    age : 18,
+    Marks:74,
+}
+
+console.log(student)
+
+console.log(student.Name, student.age)
+
+student.age= student.age-5
+console.log(student.age)
