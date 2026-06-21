1fe340f948c1d837a9ed5c41766ff92b23dbe750
Saksham Narang <sakshamnarang961@gmail.com>
2026-06-21 15:45:09 +0530

    Add files via upload

---
+++ template.js
@@ -0,0 +1,22 @@
+console.log("saksham\nnarang") // \n = next line 
+
+ console.log("saksham\tnarang") // \t =  tab space 
+
+ let str = "saksham\tnarang"
+ console.log(str.length)  //14 because\t consider 1 
+
+str1  = "porsche"
+console.log(str1.toUpperCase()) // PORSCHE
+
+
+str2 = "PORSCHE"
+console.log(str2.toLowerCase()) // porshe 
+
+str3 = "  porsche  "
+console.log(str3.trim())  // remove spaces 
+
+str4 = "mercedes"
+console.log(str4.slice(1,7)) // ercedes
+
+str5 = "mercedes"
+console.log(str5.replace("e", "l"))  // mlrcedes and replaceAll = mlrcldls 
