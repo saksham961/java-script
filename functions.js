ea9b7f7154922ded38a9b554f96cb2e03535a0fc
Saksham Narang <sakshamnarang961@gmail.com>
2026-06-21 15:46:11 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,11 @@
+function sum (x,y){
+    a = x+y
+    return a
+}
+
+ let x =  Number(prompt("Enter number :"))
+ let y = Number(prompt("Enter second number :"))
+
+let s = sum(x,y)
+console.log(s)
+
